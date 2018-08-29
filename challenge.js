c3c2838ded1cfe3170ec6da96f0d3a521805e866
Wayne Collier <ke4tri@gmail.com>
2018-08-29 11:16:07 -0500

    Added everything to the DOM, now need to order it better

--- challenge.js
+++ challenge.js
@@ -18,28 +18,28 @@ let elizabethSanger = {
     ],
     volunteers: [
       {
-        name: 'Callan',
-        address: '500 interstate blvd S',
-        email: 'callan@yomamma.com',
-        phone: '1234567890',
-        availability: '9-5',
-        activities: 'phone calls only'
+        name: ' Callan ',
+        address: ' 500 interstate blvd S ',
+        email: ' callan@yomamma.com ',
+        phone: ' 1234567890 ',
+        availability: ' 9-5 ',
+        activities: ' phone calls only '
       },
       {
-        name: 'Lauren',
-        address: '1 main street',
-        email: 'lauren@comcast.net',
-        phone: '1134567890',
-        availability: 'never',
-        activities: 'collecting money'
+        name: ' Lauren ',
+        address: ' 1 main street ',
+        email: ' lauren@comcast.net ',
+        phone: ' 1134567890 ',
+        availability: ' never ',
+        activities: ' collecting money '
       },
       {
-        name: 'Bernard',
-        address: '1 Bernard Way',
-        email: 'Bernard@thebear.net',
-        phone: '1134567890',
-        availability: 'all day',
-        activities: 'everything'
+        name: ' Bernard ',
+        address: ' 1 Bernard Way ',
+        email: ' Bernard@thebear.net ',
+        phone: ' 1134567890 ',
+        availability: ' all day ',
+        activities: ' everything '
       }
     ],
     biography: "I'm so cool!  please elect me",
@@ -74,24 +74,80 @@ let elizabethSanger = {
     selectedDiv.innerHTML = stringToPrint; // take out the + so you only have one line
 };
 
-
-
-
-
+const statementsStringBuilder = () => {
+    let newString = '';
+    for(let i=0; i<elizabethSanger.statements.length; i++) {
+       // newString += elizabethSanger.statements[i].statement;// All of the newStrings below could have been one line
+        newString += `<div class="statement">`// They were broken into multi lines for ease of reading and 
+        newString += `<h3>${elizabethSanger.statements[i].statement}</h3}`// To just make it look better
+        newString += `<h6>${elizabethSanger.statements[i].category}</h6}` // leaving the close sign and placing the bracket keeps everything on the one line
+    }
+    printToDom(newString,'statements');
+};
 
 const donationFormStringBuilder = () => {
     const newString = `<a href="https://${elizabethSanger.donationFormUrl}" target="_blank">Donations</a>`;
     printToDom(newString,'donationForm');
-}
+};
 
-  const voterRegistrationStringBuilder = () => {
-    const newString = `<a href="https://${elizabethSanger.voterRegistrationUrl}" target="_blank">Register to Vote Here</a>`;
-    printToDom(newString,'voterRegistration');
-  };
+const voterRegistrationStringBuilder = () => {
+const newString = `<a href="https://${elizabethSanger.voterRegistrationUrl}" target="_blank">Register to Vote Here</a>`;
+printToDom(newString,'voterRegistration');
+};
+
+const eventsStringBuilder = () => {
+    let newString = '';
+    for(i=0;i<elizabethSanger.events.length;i++){
+        newString += `<div class="event">`
+        newString += `<h3>${elizabethSanger.events[i].date}</h3}`
+        newString += `<h3>${elizabethSanger.events[i].title}</h3}`
+        newString += `<h3>${elizabethSanger.events[i].description}</h3}`
+        printToDom(newString, 'events');
+    };
+};
+
+const volunteersStringBuilder = () => {
+    let newString = '';
+    for(i=0;i<elizabethSanger.volunteers.length;i++){
+        newString += `<div class="volunteers">`
+        newString += `<h3>${elizabethSanger.volunteers[i].name}</h3}`
+        newString += `<h3>${elizabethSanger.volunteers[i].phone}</h3}`
+        newString += `<h3>${elizabethSanger.volunteers[i].availability}</h3}`
+        newString += `<h3>${elizabethSanger.volunteers[i].activities}</h3}`
+        printToDom(newString, 'volunteers');
+    };
+};
+
+const bioFormStringBuilder = () => {
+    const newString = `<p>${elizabethSanger.biography}</p>`;
+    printToDom(newString,'biography');
+};
+
+const imagesStringBuilder = () => {
+    let newString = '';
+    for(i=0;i<elizabethSanger.images.length;i++){
+        newString += `<div class="images">`
+        newString += `<img src="${elizabethSanger.images[i].imageUrl}">`
+        printToDom(newString, 'images');
+    };
+};
+
+const missionStatementSBuilder = () => {
+    newUrl = `<p>${elizabethSanger.missionStatement}</p>`;
+    printToDom(newUrl,'missionStatement');
+};
+
+const updateVoterRegistration = (newUrl) => {
+    elizabethSanger.voterRegistrationUrl = newUrl;
+    voterRegistrationStringBuilder();
+};
 
 
-
-
-
-  donationFormStringBuilder();
-  voterRegistrationStringBuilder();
+donationFormStringBuilder();
+voterRegistrationStringBuilder();
+statementsStringBuilder();
+eventsStringBuilder();
+volunteersStringBuilder();
+bioFormStringBuilder();
+imagesStringBuilder();
+missionStatementSBuilder();
